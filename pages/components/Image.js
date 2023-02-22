cd4a1b89a8c827b36335857c046aadd9baef2e82
Ricky Landry <0xXerkan@gmail.com>
2023-02-22 10:40:30 -0500

    Initial commit

---
+++ pages/components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+	<img src='https://cdn.dribbble.com/users/1008875/screenshots/3182998/media/89c4f34396664b97bd971361a041db84.png?compress=1&resize=400x300&vertical=top' />
+);
+
+export default Image
